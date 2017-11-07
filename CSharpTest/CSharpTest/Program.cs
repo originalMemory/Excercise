01c86cd13ae6d48fdc0d693c08f2a01061f87d02
DESKTOP-MG9HM00\Illusion <kdi1994@163.com>
2017-11-07 09:19:36 +0800

    1、BOT报错邮箱提示 2、post接口调用

--- CSharpTest/CSharpTest/Program.cs
+++ CSharpTest/CSharpTest/Program.cs
@@ -25,6 +25,7 @@ using NPOI.SS.UserModel;
 using NPOI.SS.Util;
 using AISSystem;
 
+
 namespace CSharpTest
 {
     class Program
@@ -32,15 +33,22 @@ namespace CSharpTest
         
         static void Main(string[] args)
         {
-            string url = "http://localhost:2022/api/WxPublicPay/getCode";
+            string url = "http://localhost:2022/api/Account/Regist";
             Dictionary<string, string> para = new Dictionary<string, string>();
+            para.Add("uName", "213142");
+            para.Add("uPwd1", "213142");
+            para.Add("uPwd2", "213142");
+            para.Add("email", "34234234234@qq.com");
+            para.Add("ip", "119.253.58.170");
             var response = WebApiInvoke.CreatePostHttpResponse(url, para);
             StreamReader sr = new StreamReader(response.GetResponseStream());
             Console.WriteLine(sr.ReadToEnd());
+
 
             Console.ReadKey();
         }
 
     }
+
 
 }
